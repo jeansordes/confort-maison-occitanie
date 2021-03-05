f181041de98c79b838667e7bebbc196e77ac33ed
Jean Z. SORDES <john.sordes@gmail.com>
2021-03-05 17:03:24 +0100

    reset script improved

--- reset_db.php
+++ reset_db.php
@@ -1,27 +1,24 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/sql-utilities.php';
-// dotenv
 (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__ . '/.env');
 
-global $db;
 global $connexion_string;
-$db = getPDO();
 $connexion_string = "mysql --user=" . $_ENV['db_username'] . " -p" . $_ENV['db_password'] . " " . $_ENV['db_name'];
 
-function runFile($filename) {
-    global $db;
+function runFile($filename)
+{
     global $connexion_string;
-    echo "\n--- $filename ---\n";
-    $reqString = file_get_contents(__DIR__ . '/sql/' . $filename);
-    $reqString = str_replace('cmo_db_name', $_ENV['db_name'], $reqString);
-    if ($db->query($reqString)) {
-        echo 'Success';
-    } else {
-        echo "Something went wrong, second method used\n";
-        $res = exec($connexion_string . ' -e "source ./sql/init_struct_fn_data.sql"');
-        echo $res;
-    }
+    echo "--- $filename ---\n";
+    $tmpString = file_get_contents(__DIR__ . '/sql/' . $filename);
+    $tmpString = str_replace('cmo_db_name', $_ENV['db_name'], $tmpString);
+
+    $temp = tmpfile();
+    fwrite($temp, $tmpString);
+    $res = exec($connexion_string . ' -e "source ' . stream_get_meta_data($temp)['uri'] . '"');
+    echo $res;
+    fclose($temp);
+
+    echo "\n";
 }
 
 runFile('init_struct_fn_data.sql');
