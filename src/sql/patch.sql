c351c53a69b7c36558cf568528ae109a2d8ceccc
Jean Z. SORDES <john.sordes@gmail.com>
2021-07-19 21:53:29 +0200

    patch.sql updated

--- src/sql/patch.sql
+++ src/sql/patch.sql
@@ -1,6 +1,9 @@
-create or replace view logs_enriched as
-    select l.*, p.*, nullif((select u.user_role from utilisateurs u where p.id_personne = u.id_utilisateur),'client') personne_role
-    from logs_dossiers l, personnes p where p.id_personne = l.id_utilisateur
-    order by date_heure;
+create or replace view dossiers_enriched as
+    select a.id_commercial, c.nom_produit, b.*,
+           (select date_heure from logs_dossiers l where l.id_dossier = b.id_dossier order by date_heure desc limit 1) date_creation,
+           c.id_fournisseur, d.role_responsable_etape, d.phase_etape
+    from clients_des_commerciaux a, dossiers b, produits c, etats_workflow d
+    where a.id_client = b.id_client and b.id_produit = c.id_produit and d.id_etat = b.etat_workflow_dossier
+    order by d.phase_etape desc;
     
 select 'Patch done';
