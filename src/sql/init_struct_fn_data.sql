dc958c5b3f35df8eb9bcf379e8ed516be95d3e98
Jean Z. SORDES <john.sordes@gmail.com>
2021-07-19 13:55:38 +0200

    anonymiser titre-personne.html.twig

--- src/sql/init_struct_fn_data.sql
+++ src/sql/init_struct_fn_data.sql
@@ -354,7 +354,8 @@ create or replace view fichiers_enriched as
     where a.id_fichier = b.id_fichier or a.id_fichier = c.id_fichier;
 
 create or replace view logs_enriched as
-    select l.*, p.*
+    select l.*, p.*,
+    nullif((select u.user_role from utilisateurs u where p.id_personne = u.id_utilisateur),'client') personne_role
     from logs_dossiers l, personnes p where p.id_personne = l.id_utilisateur
     order by date_heure;
 
--- src/sql/patch.sql
+++ src/sql/patch.sql
@@ -1,2 +1,6 @@
-
+create or replace view logs_enriched as
+    select l.*, p.*, nullif((select u.user_role from utilisateurs u where p.id_personne = u.id_utilisateur),'client') personne_role
+    from logs_dossiers l, personnes p where p.id_personne = l.id_utilisateur
+    order by date_heure;
+    
 select 'Patch done';
